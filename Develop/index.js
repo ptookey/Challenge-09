cd30c03d502bedc39eab40d99660fd489180fa5a
tookey95 <petetookey@gmail.com>
2023-06-15 15:17:50 -0400

    added questions to questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,10 @@
 // TODO: Include packages needed for this application
+const fs=require("fs");
+
+const inquirer=require("inquirer")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{type:"input", name:"projectTitle",message:"What is the title of your project?"},{type:"input",name:"projectDescription",message:"Describe your Project"},{type:"input",name:"projectInstall",message:"Explain how to install your project"},{type:"input",name:"projectUsage",message:"Explain how your project can be used"},{type:"list",name:"projectLicense",message:"Choose a license to use",choices:["this one","that one","what is a license"]},{type:"input",name:"projectContribution",message:"Explain how to contribute to the project via well formed pull requests and opening useful issues"},{type:"input",name:"projectTests",message:"Include any tests you may have used to fully test your project"},{type:"input",name:"githubUser",message:"What is your github username?"},{type:"input",name:"userEmail",message:"What is your email address?"},];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
